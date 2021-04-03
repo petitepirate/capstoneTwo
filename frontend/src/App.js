384211a65d725cbb55f4120eb55052f34eea72a3
megan <msmeganmcmanus@gmail.com>
2021-04-03 09:29:46 -0700

    start basic frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import useLocalStorage from "./hooks/useLocalStorage";
+import Navigation from "./navroutes/Navigation";
+import Routes from "./navroutes/Routes";
+// import LoadingSpinner from "./common/LoadingSpinner";
+import BookappApi from "./api/api";
+import UserContext from "./auth/UserContext";
+import jwt from "jsonwebtoken";
+
+// Key name for storing token in localStorage for "remember me" re-login
+export const TOKEN_STORAGE_ID = "bookapp-token";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [infoLoaded, setInfoLoaded] = useState(false);
+  // const [applicationIds, setApplicationIds] = useState(new Set([]));
+  const [currentUser, setCurrentUser] = useState(null);
+  const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
+
+  console.debug(
+      "App",
+      "infoLoaded=", infoLoaded,
+      "currentUser=", currentUser,
+      "token=", token,
   );
+
+  
+  // Load user info from API. Until a user is logged in and they have a token,
+  // this should not run. It only needs to re-run when a user logs out, so
+  // the value of the token is a dependency for this effect.
+
+  useEffect(function loadUserInfo() {
+    console.debug("App useEffect loadUserInfo", "token=", token);
+
+    async function getCurrentUser() {
+      if (token) {
+        try {
+          let { username } = jwt.decode(token);
+          // put the token on the Api class so it can use it to call the API.
+          BookappApi.token = token;
+          let currentUser = await BookappApi.getCurrentUser(username);
+          setCurrentUser(currentUser);
+          // setApplicationIds(new Set(currentUser.applications));
+        } catch (err) {
+          console.error("App loadUserInfo: problem loading", err);
+          setCurrentUser(null);
+        }
+      }
+      setInfoLoaded(true);
+    }
+
+    // set infoLoaded to false while async getCurrentUser runs; once the
+    // data is fetched (or even if an error happens!), this will be set back
+    // to false to control the spinner.
+    setInfoLoaded(false);
+    getCurrentUser();
+  }, [token]);
+
+  /** Handles site-wide logout. */
+  function logout() {
+    setCurrentUser(null);
+    setToken(null);
+  }
+
+  /** Handles site-wide signup.
+   *
+   * Automatically logs them in (set token) upon signup.
+   *
+   * Make sure you await this function and check its return value!
+   */
+  async function signup(signupData) {
+    try {
+      let token = await BookappApi.signup(signupData);
+      setToken(token);
+      return { success: true };
+    } catch (errors) {
+      console.error("signup failed", errors);
+      return { success: false, errors };
+    }
+  }
+
+  /** Handles site-wide login.
+   *
+   * Make sure you await this function and check its return value!
+   */
+  async function login(loginData) {
+    try {
+      let token = await BookappApi.login(loginData);
+      setToken(token);
+      return { success: true };
+    } catch (errors) {
+      console.error("login failed", errors);
+      return { success: false, errors };
+    }
+  }
+
+  
+  return (
+    <BrowserRouter>
+      <UserContext.Provider
+          value={{ currentUser, setCurrentUser }}>
+        <div className="App">
+          <Navigation logout={logout} />
+          <Routes login={login} signup={signup} />
+        </div>
+      </UserContext.Provider>
+    </BrowserRouter>
+);
 }
 
 export default App;
